d6f2baad250d99f80bed5ce7fe32929d03893b7d
FoperGT <fopergt@gmail.com>
2025-05-12 22:48:14 +1000

    [WIP]

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from posts.models import Comment, Post, Group, Follow
+from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueTogetherValidator
 
 
-from posts.models import Comment, Post
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -17,7 +20,45 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='Нельзя подписаться на автора повторно!'
+            )
+        ]
+
+    def validate_following(self, value):
+        user = self.context.get('request').user
+        if user == value:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя'
+            )
+        return value
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,17 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router = SimpleRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register('groups', GroupViewSet)
+router.register('comments', CommentViewSet)
+router.register(r'follow', FollowViewSet, basename='follow')
+
 
 urlpatterns = [
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.jwt')),
+    path('', include(router.urls)),
 ]
